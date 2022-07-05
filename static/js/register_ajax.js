ea51dd23a6e2463181b8c6e0f1087b6150e88ea2
mangoit-abhi <108114731+mangoit-abhi@users.noreply.github.com>
2022-07-05 19:02:50 +0530

    Add files via upload

--- static/js/register_ajax.js
+++ static/js/register_ajax.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
                         }
                         else if(data == false)
                         {
-                            window.location = '/';
+                            window.location = '/editor';
                         }
                     }
             });
